6cabb1cc5f4c34d99b51f0af50407056895ec8e3
efactguest <vlrprem88@gmail.com>
2013-05-26 17:35:24 +0530

    Include Security Parameter

---
+++ eFact.BLL/SecurityParameter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace eFact.BLL
+{
+    public class SecurityParameter
+    {
+        public int PasswordExpireDate { get; set; }
+        public int MaxLoginLimit { get; set; }
+        public bool FourEyePolicy { get; set; }
+
+        string connStr = ConfigurationManager.ConnectionStrings["DBconnStr"].ToString();
+
+        public SecurityParameter SecurityParameterGet()
+        {
+            SqlConnection sqlConnection = new SqlConnection(connStr);
+            SqlDataReader sqlReader;
+            SecurityParameter securityParameter = new SecurityParameter();
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                }
+
+                SqlCommand sqlCommand = new SqlCommand("usp_SecurityParameter_Get", sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlReader = sqlCommand.ExecuteReader();
+                while (sqlReader.Read())
+                {
+                    securityParameter = new SecurityParameter
+                    {
+                        PasswordExpireDate = Convert.ToInt32(sqlReader["PasswordExpireDays"]),
+                        MaxLoginLimit = Convert.ToInt32(sqlReader["NumberOfInvalidLoging"]),
+                        FourEyePolicy = Convert.ToBoolean(sqlReader["FourEyePolicy"])
+                    };
+                }
+                return securityParameter;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
